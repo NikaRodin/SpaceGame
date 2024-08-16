8fdc557e98330972d63c4c9abc7fa7a00e5b2b7e
Nika <nikolina.rodin@hotmail.com>
2024-08-16 22:23:36 +0200

    Added breakable space waste

--- Assets/MeteorPistol.cs
+++ Assets/MeteorPistol.cs
@@ -7,6 +7,11 @@ using UnityEngine.XR;
 public class MeteorPistol : MonoBehaviour
 {
     public ParticleSystem particles;
+    public LayerMask layerMask;
+    public Transform shootSource;   //starting point from the raycast
+    public float distance = 10;     //max distance of raycast
+
+    private bool rayActivated = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +25,39 @@ public class MeteorPistol : MonoBehaviour
     public void StartShoot()
     {
         particles.Play();
+        rayActivated = true;
     }
 
     public void StopShoot()
     { 
         particles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        rayActivated = false;
     }
 
     // Update is called once per frame
     void Update()
+    {   
+        if(rayActivated)
+        {
+            RaycastCheck();
+        }
+    }
+
+    void RaycastCheck()
     {
-        
+        RaycastHit hit;
+
+        bool hasHit = Physics.Raycast(
+            shootSource.position, 
+            shootSource.forward, 
+            out hit, 
+            distance, 
+            layerMask
+        );
+
+        if (hasHit) 
+        {
+            hit.transform.gameObject.SendMessage("Break", SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
